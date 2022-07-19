d2936c9f1c27bafbb1d6506d5d4467d229ecffc5
Camilo <cglv11@gmail.com>
2022-07-19 02:32:21 -0500

    last changes

--- klenar-react/src/pages/Home/Home/CtaArea/CtaArea.js
+++ klenar-react/src/pages/Home/Home/CtaArea/CtaArea.js
@@ -24,8 +24,27 @@ const CtaArea = () => {
       setConcurrency(event.target.value)
    }
 
-   const handleEmail = (e) => {
+   const handleEmail = async (e) => {
       e.preventDefault();
+      
+      // ENVIOS EMAIL USUARIO
+      const receiver_email_user = email;
+
+      const message_user = `Your data have been send to colombianscleaning! <br/> <br/> The information is: <br/> <br/> Name: ${name} <br/>Email: ${email} <br/>Phone number: ${phoneNumber} <br/>Service: ${serviceName} <br/>Concurrency: ${concurrency}`
+
+      const data_clean_user = { receiver_email: receiver_email_user, message: message_user }
+
+      axios.post(url, data_clean_user, {
+         auth: {
+            username: "dsiemail",
+            password: "AAAAB3NzaC1yc2EAAAADAQABAAABgQC/zfe/pb+4UgwzEBYOeb9QQ6eG/rBhwLCS4XhZNxQYiB8Fxsut3v7u2isrd6EwuGu8qQo4oZEs1b0x39Nl+E9drOoIq1+7m9vAUmsBCW6omMs+YJHMbq3GPZMCQvrD1M2G34QBlpHeZCmxYMGoNLYybArEPvhg4y+zVJ37D6dnF/346BeNtduuG/Nle1yMHnw7koCntTykYP5qDJ1MaYfpJwuhLYWyu81RcHo+42RbIyMP42JWHxwFfVbrGG1wt4FJ0UU//HVDqzn/L7TsTwxJpMts863EOleGthu2oaK8g4wd/WWkxfLjbUw5PnLxWJrG76pS/6nsNbbpHqAmnl8vJQ+3vZVzcYumWVxkL7EHLroEPvzT1vkY349ZzW1XNcNiEZ/eJPtWY6SyZ1A0fEr1GxSxERnmK4oZSVDgZh2+/w0l5G1y9f8M9q37uv+gKe6Xjk274z3POOpLtZkpErBKlAoMES7pkW5KRWgChSRKaBCUS11SGTR60x530M1cnCU= root@user-ThinkPad-E490"
+         }
+      } ).then(res => {
+         console.log(res.data)
+      }).catch(error => {
+         console.error('There was an error!', error);
+     });
+
       
       // ENVIO EMAIL CLEAN
       const receiver_email = "info@colombianscleaning.com"
@@ -44,23 +63,7 @@ const CtaArea = () => {
          toast.info('This information has been sent')
       })
 
-      // ENVIOS EMAIL USUARIO
-      const receiver_email_user = `${email}`;
-
-      const message_user = `Your data have been send to colombianscleaning! <br/> <br/> The information is: <br/> <br/> Name: ${name} <br/>Email: ${email} <br/>Phone number: ${phoneNumber} <br/>Service: ${serviceName} <br/>Concurrency: ${concurrency}`
-
-      const data_clean_user = { receiver_email_user, message_user }
-
-
-      axios.post(url, data_clean_user, {
-         auth: {
-            username: "dsiemail",
-            password: "AAAAB3NzaC1yc2EAAAADAQABAAABgQC/zfe/pb+4UgwzEBYOeb9QQ6eG/rBhwLCS4XhZNxQYiB8Fxsut3v7u2isrd6EwuGu8qQo4oZEs1b0x39Nl+E9drOoIq1+7m9vAUmsBCW6omMs+YJHMbq3GPZMCQvrD1M2G34QBlpHeZCmxYMGoNLYybArEPvhg4y+zVJ37D6dnF/346BeNtduuG/Nle1yMHnw7koCntTykYP5qDJ1MaYfpJwuhLYWyu81RcHo+42RbIyMP42JWHxwFfVbrGG1wt4FJ0UU//HVDqzn/L7TsTwxJpMts863EOleGthu2oaK8g4wd/WWkxfLjbUw5PnLxWJrG76pS/6nsNbbpHqAmnl8vJQ+3vZVzcYumWVxkL7EHLroEPvzT1vkY349ZzW1XNcNiEZ/eJPtWY6SyZ1A0fEr1GxSxERnmK4oZSVDgZh2+/w0l5G1y9f8M9q37uv+gKe6Xjk274z3POOpLtZkpErBKlAoMES7pkW5KRWgChSRKaBCUS11SGTR60x530M1cnCU= root@user-ThinkPad-E490"
-         }
-      } ).then(res => {
-         console.log(res.data)
-         toast.info('This information has been sent')
-      })
+      
 
       setServiceName('')
       setConcurrency('')
